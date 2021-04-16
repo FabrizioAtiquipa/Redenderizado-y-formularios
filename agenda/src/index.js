b261fac0707809695a16b50643cbb217bc5ff11b
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-04-15 22:15:04 -0500

    2. Agenda - No nombres repetidos

--- agenda/src/index.js
+++ agenda/src/index.js
@@ -1,59 +1,60 @@
 import ReactDOM from "react-dom";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-    const [ persona, setPersona ] = useState([
-        { nombre: 'Arto Hellas' }
-    ])
-    const [ nuevonom, setNuevoNombre ] = useState('')
+  const [persona, setPersona] = useState([{ nombre: "Arto Hellas" }]);
+  const [nuevonom, setNuevoNombre] = useState("");
 
-    const Guardar=(event)=>{
-        event.preventDefault()
+  const Guardar = (event) => {
+    event.preventDefault();
 
-        const nuevaPersona={
-          nombre:nuevonom
-        }
+    const nuevaPersona = {
+      nombre: nuevonom,
+    };
 
-        setPersona([
-            ...persona,nuevaPersona,
-        ])
 
-        setNuevoNombre('')
 
+
+    let respetidos = persona.filter((person) => {
+      return person.nombre === nuevonom;
+    });
+
+    console.log(respetidos);
+
+    if (respetidos.length > 0) {
+      alert(`${nuevaPersona.nombre} ya existe `);
+      return;
     }
 
-    const Agregar=(event)=>{
-      setNuevoNombre(event.target.value)
-    }
+    setPersona([...persona, nuevaPersona]);
 
-    return (
+    setNuevoNombre("");
+  };
+
+  const Agregar = (event) => {
+    setNuevoNombre(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
         <div>
-            <h2>Phonebook</h2>
-            <form>
-                <div>
-                    name: <input type="text" onChange={Agregar} value={nuevonom} />
-                </div>
-                <div>
-                    <button onClick={Guardar}>add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <ul>
-                {
-                    persona.map((person, index) => {
-                      const {nombre} = person
-                        return (
-                            <li key={nombre}>{nombre}</li>
-                        );
-
-                    })
-
-                }
-            </ul>
-
-
+          name: <input type="text" onChange={Agregar} value={nuevonom} />
         </div>
-    )
-}
+        <div>
+          <button onClick={Guardar}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persona.map((person, index) => {
+          const { nombre } = person;
+          return <li key={nombre}>{nombre}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
