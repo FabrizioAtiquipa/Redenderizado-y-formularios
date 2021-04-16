3ac79e43ff4ee3219c9e854e21d76af219d86c9f
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-04-15 23:05:02 -0500

    2. Agenda - Filtro

--- agenda/src/index.js
+++ agenda/src/index.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 const App = () => {
   const [nuevonom, setNuevoNombre] = useState("");
   const [nuevonum, setNuevoNumero] = useState("");
+  const [filtro, setFiltro] = useState("");
   const [persona, setPersona] = useState([
     { nombre: "Arto Hellas", numero: "040-1234567" },
   ]);
@@ -40,16 +41,36 @@ const App = () => {
     setNuevoNumero(event.target.value);
   };
 
+  let filtroPersona = persona;
+
+  if (filtro.length > 0) {
+    filtroPersona = persona.filter((person) => {
+      return person.nombre.toUpperCase().includes(filtro);
+    });
+  }
+
+  const Buscar = (event) => {
+    setFiltro(event.target.value.toUpperCase());
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
+          filter shown with:{" "}
+          <input type="text" onChange={Buscar} value={filtro} />
+        </div>
+        <hr />
+        <h2>add a new</h2>
+
+        <div>
           name: <input type="text" onChange={Agregar} value={nuevonom} />
         </div>
 
         <div>
-          number:<input type="text" onChange={Agregarnum} value={nuevonum} />
+          number:
+          <input type="text" onChange={Agregarnum} value={nuevonum} />
         </div>
 
         <div>
@@ -58,9 +79,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persona.map((person, index) => {
-          const { nombre, numero } = person;
-          return <li key={nombre}>{nombre} {numero}</li>;
+        {filtroPersona.map((person, index) => {
+          const {nombre, numero} = person
+          return (
+            <li key={nombre}> {nombre} {numero}</li>
+          );
         })}
       </ul>
     </div>
