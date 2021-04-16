bf6737b72f5b182a2cafc729bd1272979b049b3e
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-04-15 19:46:07 -0500

    1. Cursos-Informacion del curso

---
+++ cursos/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//Half Stack application development
+export default  function  Header(props) {
+    const { title } = props
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
